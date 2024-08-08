f907975b128a1ce60f149cf4fafeabdd8458bd68
raccococococo <sirio.pastori@vegans.it>
2024-08-08 15:52:49 +0200

    remove servo

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -69,25 +69,6 @@ def generate_launch_description():
     )
 
 
-    # Code for delaying a node (I haven't tested how effective it is)
-    # 
-    # First add the below lines to imports
-    # from launch.actions import RegisterEventHandler
-    # from launch.event_handlers import OnProcessExit
-    #
-    # Then add the following below the current diff_drive_spawner
-    # delayed_diff_drive_spawner = RegisterEventHandler(
-    #     event_handler=OnProcessExit(
-    #         target_action=spawn_entity,
-    #         on_exit=[diff_drive_spawner],
-    #     )
-    # )
-    #
-    # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
-
-
-
-    # Launch them all!
     return LaunchDescription([
         rsp,
         delayed_controller_manager,
